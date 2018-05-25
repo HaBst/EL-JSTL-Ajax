7ff9b993671db666acc793aa2874774475e9c584
HaBst <ylk2305@naver.com>
2018-05-25 18:45:45 +0900

    a

---
+++ web2/src/member/controller/UpdateServlet.java
@@ -0,0 +1,60 @@
+package member.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import member.model.service.MemberService;
+import member.model.vo.Member;
+
+/**
+ * Servlet implementation class UpdateServlet
+ */
+@WebServlet(name = "Update", urlPatterns = { "/update" })
+public class UpdateServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UpdateServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession(false);
+		
+		int result = 0;
+		
+		if(session.getAttribute("user") !=null) {
+			result = new MemberService().memberUpdate(((Member)session.getAttribute("user")));
+			if(result>0) {
+				System.out.println("업데이트 성공");
+			}else {
+				System.out.println("업데이트 실패");
+			}
+			
+		}else {// 로그인을 하지않고 요청했을 경우(비정상 접근)
+			response.sendRedirect("/views/member/myPageError");
+		}	
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
